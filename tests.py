8cbb722cfddd0db56c42debbe80d8a7161f9c9ab
Sergei Orlov <sergeio@aweber.com>
2012-10-09 20:39:31 -0400

    tests: Use unittest.TestCase
    But for the record, rolling your own is pretty fun

--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+from unittest import TestCase, main
 from integrate import (
     get_min_max_for_func,
     make_random_points,
@@ -9,67 +10,72 @@ from integrate import (
 def linear(x):
     return x
 
-def assert_integration_correct(
-        func, xmin, xmax, expected, error, num_points=10000):
-    result = monte_carlo_integrate(func, xmin, xmax, num_points=num_points)
-    if abs(result - expected) > error:
-        raise AssertionError('Expected {0}; got {1}'.format(expected, result))
 
-def test_compare_function_1_value():
-    assert num_under_curve(linear, [(1, 0)]) == 1
+####
+##
+## num_under_curve
+##
+####
 
+class WhenEvaluatingPointsUnderCurve(TestCase):
 
-def test_compare_function_multiple_values_all_under_curve():
-    assert num_under_curve(linear, [(1, 0), (.5, .4), (2, 1.3)]) == 3
+    def test_compare_function_1_value(self):
+        assert num_under_curve(linear, [(1, 0)]) == 1
 
+    def test_compare_function_multiple_values_all_under_curve(self):
+        assert num_under_curve(linear, [(1, 0), (.5, .4), (2, 1.3)]) == 3
 
-def test_compare_function_multiple_values_some_under_curve():
-    assert num_under_curve(linear, [(1, 0), (.5, .4), (2, 3)]) == 2
+    def test_compare_function_multiple_values_some_under_curve(self):
+        assert num_under_curve(linear, [(1, 0), (.5, .4), (2, 3)]) == 2
 
+    def test_make_random_points_returns_correct_number_results(self):
+        assert len(
+            list(make_random_points((0, 0), (1, 1), num_points=10))) == 10
 
-def test_make_random_points_returns_correct_number_results():
-    assert len(list(make_random_points((0, 0), (1, 1), num_points=10))) == 10
-
-
-def test_make_random_points_returns_all_valid_points():
-    for x, y in make_random_points((0, -3), (10, -1), num_points=30):
-        assert 0 <= x <= 10 and -3 <= y <= -1
-
-
-def test_integrate_linear_func_zero_to_one():
-    assert_integration_correct(linear, 0, 1, .5, .02, num_points=10000)
+    def test_make_random_points_returns_all_valid_points(self):
+        for x, y in make_random_points((0, -3), (10, -1), num_points=30):
+            assert 0 <= x <= 10 and -3 <= y <= -1
 
 
-def test_integrate_linear_func_zero_to_two():
-    assert_integration_correct(linear, 0, 2, 2, .05, num_points=10000)
+####
+##
+## monte_carlo_integrate
+##
+####
+
+class WhenTestingIntegrationFunction(TestCase):
+
+    def assert_integration_correct(
+            self, func, xmin, xmax, expected, error, num_points=10000):
+        result = monte_carlo_integrate(func, xmin, xmax, num_points=num_points)
+        if abs(result - expected) > error:
+            raise AssertionError(
+                'Expected {0}; got {1}'.format(expected, result))
+
+    def test_integrate_linear_func_zero_to_one(self):
+        self.assert_integration_correct(linear, 0, 1, .5, .02)
+
+    def test_integrate_linear_func_zero_to_two(self):
+        self.assert_integration_correct(linear, 0, 2, 2, .05)
+
+    def test_integrate_linear_func_neg_one_to_two(self):
+        self.assert_integration_correct(
+            linear, -1, 2, 1.5, .05, num_points=100000)
 
 
-def test_integrate_linear_func_neg_one_to_two():
-    assert_integration_correct(linear, -1, 2, 1.5, .05, num_points=100000)
+####
+##
+## get_min_max_for_func
+##
+####
 
+class WhenTestingGetMinMaxFunction(TestCase):
 
-def test_getting_min_max_for_linear_func():
-    x, y = get_min_max_for_func(linear, 0, 1)
-    assert x == 0
-    assert abs(1 - y) < .000001
-
-
-def run_tests():
-    print '\nRunning tests:'
-    passed = 0
-    total = 0
-    for key, value in globals().items():
-        if key.startswith('test_'):
-            total += 1
-            try:
-                value()
-                print '.',
-                passed += 1
-            except AssertionError as e:
-                print 'F', key, e
-
-    print '\nPassed {0} / {1} tests.'.format(passed, total)
+    def test_getting_min_max_for_linear_func(self):
+        x, y = get_min_max_for_func(linear, 0, 1)
+        assert x == 0
+        assert abs(1 - y) < .000001
 
 
 if __name__ == '__main__':
-    run_tests()
+    main()
